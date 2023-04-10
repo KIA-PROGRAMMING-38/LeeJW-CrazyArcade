132815f8ce79daef6bb5426c82dc31d5e50091d1
leejw <krebye@gmail.com>
2023-04-10 17:55:01 +0900

    [Feat] : 싱글톤 패턴을 이용해 맵 매니저 구현

---
+++ CopyCreateCrazyArcade/Assets/Script/MapManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+namespace Assets.Script
+{
+
+    public class MapManager : MonoBehaviour
+    {
+        // 맵 매니저의 경우 어디서든 접근이 필요하기 때문에 싱글톤 사용
+        public static MapManager Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    s_instance = GameObject.FindObjectOfType<MapManager>();
+                }
+
+                return s_instance;
+            }
+        }
+
+        private static MapManager s_instance;
+        private Grid _grid;
+        
+        void Awake()
+        {
+            _grid = GetComponent<Grid>();
+|
+            if (s_instance == null)
+            {
+                s_instance = this;
+            }
+        }
+
+        Vector3 _localPosition;
+        private const float _plusPositionX = 0.42f;
+
+        public Vector3 LocalToCellPosition(Transform targetTransform)
+        {
+            _localPosition = targetTransform.position;
+
+            _localPosition.x = _localPosition.x + _plusPositionX;
+
+            return _grid.LocalToCell(_localPosition);
+
+        }
+
+    }
+}
--- CopyCreateCrazyArcade/Assets/Script/PlayerAttack.cs
+++ CopyCreateCrazyArcade/Assets/Script/PlayerAttack.cs
@@ -1,5 +1,7 @@
+using Assets.Script;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -7,57 +9,38 @@ public class PlayerAttack : MonoBehaviour
 {
     PlayerInput _input;
     private GameObject _Balloon;
-    public Grid _grid;
+
     private const float _plusPositionX = 0.42f;
     private Vector3 maxRay = new Vector3(0, 0, -1.2f);
     RaycastHit2D _raycast;
+
     private void Awake()
     {
         _input = GetComponent<PlayerInput>();
         _Balloon = Resources.Load("Balloon") as GameObject;
-        _grid = GetComponent<Grid>();
-        //_raycast = GetComponent<RaycastHit2D>();
 
     }
-    Vector3 cellpo;
-    Vector3 selfposition;
-    private void FixedUpdate()
-    {
 
-        Debug.DrawLine(transform.position + new Vector3(0, 0, -0.5f), transform.position + maxRay, Color.blue);
-        _raycast = Physics2D.Raycast(transform.position, transform.position + maxRay);
-        if (_raycast.collider != null)
-        {
-            Debug.Log($"{_raycast.collider.name}");
-        }
-        else
-        {
-            Debug.Log("널값임");
-        }
-    }
+    private Vector3 selfposition;
+
     void Update()
     {
-        // cell 사이즈에 따라 어색하게 출력되는 부분 포지션 수식으로 자연스럽게 배치.
-        selfposition = transform.position;
-        selfposition.x = selfposition.x + _plusPositionX;
-        cellpo = _grid.LocalToCell(selfposition);
-
+        // 현재 포지션을 셀포지션으로
+        selfposition = MapManager.Instance.LocalToCellPosition(transform);
+       
         if (_input.Attack())
         {
 
-            // 맵 끝 에 설치할 경우 예외처리
-            if (cellpo.x == -10f)
+            // 맵 끝 에 설치할 경우 예외처리\
+            
+            if ( selfposition.x == -10f)
             {
-                cellpo.x = -9f;
+                selfposition.x = -9f;
             }
             if (_raycast.collider.name != "Balloon(Clone)")
             {
-                Instantiate(_Balloon, cellpo, transform.rotation);
-
-
+                Instantiate(_Balloon, selfposition, transform.rotation);
             }
-
-
 
         }
 
