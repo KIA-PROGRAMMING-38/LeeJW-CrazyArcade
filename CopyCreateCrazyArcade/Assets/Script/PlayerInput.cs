bfbb3d88c90ae19b4bf09ea53f8e19709bd1e48a
leejw <krebye@gmail.com>
2023-04-17 21:44:11 +0900

    [Feat] : 플레이어 사망상태 단계별 구현 및 아이템사용 시 생존 구현

--- CopyCreateCrazyArcade/Assets/Script/PlayerInput.cs
+++ CopyCreateCrazyArcade/Assets/Script/PlayerInput.cs
@@ -1,3 +1,4 @@
+using Assets.Script;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,7 +11,11 @@ public class PlayerInput : MonoBehaviour
     public float _2PlayerPositionX { get; private set; }
     public float _2PlayerPositionY { get; private set; }
 
-
+    private PlayerStatus _status;
+    private void Awake()
+    {
+        _status = GetComponent<PlayerStatus>();
+    }
     private void Update()
     {
         _1PlayerPositionX = Input.GetAxisRaw("Horizontal");
@@ -19,10 +24,18 @@ public class PlayerInput : MonoBehaviour
         _2PlayerPositionX = Input.GetAxisRaw("SecondHorizontal");
         _2PlayerPositionY = Input.GetAxisRaw("SecondVertical");
 
+        if(_status.needleCount > 0 && FirstPlayerUseItem())
+        {
+            --_status.needleCount;
+            _status.UseNeedle();
+            
+        }
 
+     
     }
     public bool FirstPlayerAttack() => Input.GetKeyDown(KeyCode.Space);
     public bool SecondPlayerAttack() => Input.GetKeyDown(KeyCode.LeftShift);
+    public bool FirstPlayerUseItem() => Input.GetKeyDown(KeyCode.LeftControl);
 
 
 }
