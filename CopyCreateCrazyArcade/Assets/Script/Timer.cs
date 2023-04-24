14269b072b3521ae5621f084076f2cb4d987b96e
leejw <krebye@gmail.com>
2023-04-24 17:08:49 +0900

    [Feat] : Timer 구현 및 UI 업데이트 , Timer 의 시간에 따라 게임종료

---
+++ CopyCreateCrazyArcade/Assets/Script/Timer.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+    private float elapsedTime;
+    private bool timeCheck = true;
+    public GameManager _manager;
+
+    private void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+       
+    }
+
+    private void Update()
+    {
+
+        elapsedTime += Time.deltaTime;
+
+        long _rT = 180 -(long)elapsedTime;
+        DateTimeOffset _timer = DateTimeOffset.FromUnixTimeSeconds(_rT);
+
+        if(timeCheck)
+        _text.text = $"{_timer.Minute} : {_timer.Second}";
+
+        if (_timer.Minute == 0 && _timer.Second <= 0)
+        {
+            timeCheck= false ;
+            _manager.GameOver();
+        }
+    }
+}
